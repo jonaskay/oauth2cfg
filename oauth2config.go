f3634073db4969e8d55f5138ede8f360406b27e7
jonaskay <jonaskay@iki.fi>
2019-03-02 10:06:14 +0200

    Add Config interface and check that oauth2.Config implements it

--- oauth2config.go
+++ oauth2config.go
@@ -1,1 +1,16 @@
 package oauth2config
+
+import (
+	"context"
+	"net/http"
+
+	"golang.org/x/oauth2"
+)
+
+type Config interface {
+	AuthCodeURL(string, ...oauth2.AuthCodeOption) string
+	Client(context.Context, *oauth2.Token) *http.Client
+	Exchange(context.Context, string, ...oauth2.AuthCodeOption) (*oauth2.Token, error)
+	PasswordCredentialsToken(context.Context, string, string) (*oauth2.Token, error)
+	TokenSource(context.Context, *oauth2.Token) oauth2.TokenSource
+}
---
+++ oauth2config_test.go
@@ -0,0 +1,16 @@
+package oauth2config
+
+import (
+	"testing"
+
+	"golang.org/x/oauth2"
+)
+
+func TestConfigInterface(t *testing.T) {
+	c := &oauth2.Config{}
+	var i interface{} = c
+
+	if _, ok := i.(Config); ok != true {
+		t.Errorf("Given config doesn't implement Config")
+	}
+}
